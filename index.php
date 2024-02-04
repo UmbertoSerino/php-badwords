1ab6311375266c87b19b58c9703b61339e42d9a9
UmbertoSerino <serumberto@gmail.com>
2024-02-04 19:20:22 +0100

    modified error

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!-- action, una volta compilato il form e inviato rimanda al file script.php, method è il metodo con cui viene effettuata la chiamata -->
-<?php
-// Nome della variabile con il $
-$paragraph = 'Ciao, sono quello che ti ha incontrato alla festa';
-// prende il valore inserito nel form nel campo name
-$testo = $_GET['parola'];
-// str_replace, prende una stringa e la sostituisce con un altra, dalla variabile $
-$changeString = str_ireplace($testo, '***', $paragraph);
-
-?>
 <form action="index.php" method="GET">
     <label>Scrivi una parola</label>
     <!-- inseriamo il dato name, che poi verra' inviato al file script.php -->
     <input type="text" name="parola">
     <button type="submit">Risultato</button>
 </form>
+<?php
+// Nome della variabile con il $
+$paragraph = 'Ciao, sono quello che ti ha incontrato alla festa';
+// prende il valore inserito nel form nel campo name
+$testo = isset($_GET['parola']) ? $_GET['parola'] : '';
+// str_replace, prende una stringa e la sostituisce con un altra, dalla variabile $
+$changeString = str_ireplace($testo, '***', $paragraph);
+
+?>
 
 <!-- echo, stampa il valore della variabile ed echo strlen sta per stampa valore variabile e  lunghezza stringa -->
 <p><?php echo $paragraph ?> = <strong> <?php echo strlen($paragraph); ?> lettere</strong> </p>
